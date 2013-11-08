8fb8866b31b84a230760538cb0ae47f8e88287f4
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2013-11-08 11:37:23 -0600

    Corrección de fallos al ultimo cambio de la base de datos - Añadida la clase entidad EncargadoProyecto - Añadido el controllado de Empresa - Corregido los triggers en el modelo de la base de datos - Corregido y revizado el script de creación de la base de datos

---
+++ src/INTI/RegistroAcademicoBundle/Entity/EncargadoProyecto.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace INTI\RegistroAcademicoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * EncargadoProyecto
+ *
+ * @ORM\Table(name="Encargado_proyecto")
+ * @ORM\Entity
+ */
+class EncargadoProyecto
+{
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="id", type="integer", nullable=false)
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="IDENTITY")
+	 */
+	private $id;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="nombre", type="string", length=80, nullable=false)
+	 */
+	private $nombre;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="rol", type="string", length=20, nullable=true)
+	 */
+	private $rol;
+
+	/**
+	 * Get id
+	 *
+	 * @return integer
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * Set nombre
+	 *
+	 * @param string $nombre
+	 * @return EncargadoProyecto
+	 */
+	public function setNombre($nombre)
+	{
+		$this->nombre = $nombre;
+
+		return $this;
+	}
+
+	/**
+	 * Get nombre
+	 *
+	 * @return string
+	 */
+	public function getNombre()
+	{
+		return $this->nombre;
+	}
+
+	/**
+	 * Set rol
+	 *
+	 * @param string $rol
+	 * @return EncargadoProyecto
+	 */
+	public function setRol($rol)
+	{
+		$this->rol = $rol;
+
+		return $this;
+	}
+
+	/**
+	 * Get rol
+	 *
+	 * @return string
+	 */
+	public function getRol()
+	{
+		return $this->rol;
+	}
+}
