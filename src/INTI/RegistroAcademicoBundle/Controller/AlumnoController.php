d22dd094449cfabf0fab98c6da0a5b26c2a29dc4
Roberto Pineda <enri_pin@yahoo.com>
2013-11-21 05:31:23 -0600

    Actualizacion de index Alumno debido a problemas surgidos despues de mezcla de cambios

--- src/INTI/RegistroAcademicoBundle/Controller/AlumnoController.php
+++ src/INTI/RegistroAcademicoBundle/Controller/AlumnoController.php
@@ -3,6 +3,7 @@
 namespace INTI\RegistroAcademicoBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -247,31 +248,31 @@ class AlumnoController extends Controller
 			order_field, sence, n_result, search_field, search, actual, anterior, especialidad
 		*/
 		$entity='Alumno';
-		$campos=Array("u.nie","u.primerapellido","u.segundoapellido","u.nombres", "n.nombre", "");
-		$nombre_campos=Array("NIE", "Primer Apellido", "Segundo Apellido", "Nombres", "Especialidad", "Acciones");
+		$nombre_campos=Array("NIE", "Nombre", "Especialidad", "Acciones");
 		$order_field=$_REQUEST['order_field'];
 		$especialidad=$_REQUEST['especialidad'];
 		$sence=strtolower($_REQUEST['sence']);
+		$campos=Array("p.nie ".$sence, "p.primerApellido ".$sence.", p.segundoApellido ".$sence.", p.nombres ".$sence, "n.nombre ".$sence);
 		$n_result=$_REQUEST['n_result'];
 		$parameters=Array();
+		$cEspecialidad="";
+		if(($especialidad!="todas")&&($especialidad!="")){
+			$cEspecialidad.=" where n.codigo=:codigo";
+			$parameters['codigo']=$especialidad;
+		}
 		$condicion="";
 		if(isset($_REQUEST['search']))
 		if($_REQUEST['search']!=""){
+			if($cEspecialidad=="")	
+				$condicion.=" where ";
+			else
+				$condicion.=" and ";
 			$search=$_REQUEST['search'];
-			$condicion=" where CONCAT(CONCAT(CONCAT(CONCAT(u.nombres,' '),u.primerapellido),' '),u.segundoapellido) like :search or u.nie like :search2";
+			$condicion.="CONCAT(CONCAT(CONCAT(CONCAT(p.primerApellido,' '),p.segundoApellido),' '),p.nombres) like :search or p.nie like :search2";
 			$parameters['search']='%'.$search.'%';
 			$parameters['search2']=$search.'%';
 		}
-		$cEspecialidad="";
-		if(($especialidad!="todas")&&($especialidad!="")){
-			if($condicion=="")	
-				$cEspecialidad.=" where ";
-			else
-				$cEspecialidad.=" and ";
-			$cEspecialidad.="n.codigo=:codigo";
-			$parameters['codigo']=$especialidad;
-		}
-		$dql="SELECT COUNT(p) FROM RegistroAcademicoBundle:Alumno p JOIN p.nie u JOIN u.especialidad n".$condicion.$cEspecialidad;
+		$dql="SELECT COUNT(p) FROM RegistroAcademicoBundle:Alumno p JOIN p.especialidad n".$cEspecialidad.$condicion;
 		$em = $this->getDoctrine()->getManager();
 		$query=$em->createQuery($dql);
 		if(isset($_REQUEST['search']))
@@ -306,9 +307,8 @@ class AlumnoController extends Controller
 			}
 		}else	$actual=1;
 
-		$query="select p, u from RegistroAcademicoBundle:Alumno p JOIN p.nie u JOIN u.especialidad n".$condicion.$cEspecialidad;
-		if(($order_field!="")&&($sence!=""))
-			$query.=" order by ".$order_field." ".$sence;
+		$query="select p, n from RegistroAcademicoBundle:Alumno p JOIN p.especialidad n".$cEspecialidad.$condicion;
+		$query.=" order by ".$campos[$order_field];
 
 		//$text.= $query;
 		$resultado=$em->createQuery($query);
@@ -320,40 +320,40 @@ class AlumnoController extends Controller
 		$resultado->setMaxResults($n_result)->setFirstResult($init);
 		$result=$resultado->getResult();
 		
-		$text= "<table class='table'>\n<tr class='header'>";
+		$text= "<table class='table table-hover'>\n<tr class='header'>";
 		for($i=0;$i<count($campos);$i++){
-			if($campos[$i]==$order_field){
+			if($i==$order_field){
 				if($sence=="asc"){
-					$text.= "<th name='".$campos[$i]."'>".$nombre_campos[$i]." <i class='icon-chevron-up icon-white'></i></th>";
+					$text.= "<th class='".$i."'>".$nombre_campos[$i]." <i class='icon-chevron-up icon-white'></i></th>";
 				}else{
-					$text.= "<th name='".$campos[$i]."'>".$nombre_campos[$i]." <i class='icon-chevron-down icon-white'></i></th>";
+					$text.= "<th class='".$i."'>".$nombre_campos[$i]." <i class='icon-chevron-down icon-white'></i></th>";
 				}
 			}else{
-				$text.= "<th name='".$campos[$i]."'>".$nombre_campos[$i]."</th>";
+				$text.= "<th class='".$i."'>".$nombre_campos[$i]."</th>";
 			}
 		}
 		$text.= "<th class='control'></th></tr>\n";
 		if($result!=false){
 			for($i=0;$i<count($result);$i++){
 				$text.= "<tr>";
-				$text.= "<td>".$result[$i]->getNie()->getNie()."</td>";
-				$text.= "<td>".$result[$i]->getNie()->getPrimerapellido()."</td>";
-				$text.= "<td>".$result[$i]->getNie()->getSegundoapellido()."</td>";
-				$text.= "<td>".$result[$i]->getNie()->getNombres()."</td>";
-				$text.= "<td>".$result[$i]->getNie()->getEspecialidad()->getNombre()."</td>";
+				$text.= "<td>".$result[$i]->getNie()."</td>";
+				$text.= "<td>".$result[$i]->getPrimerapellido()." ".$result[$i]->getSegundoapellido().", ".$result[$i]->getNombres()."</td>";
+				$text.= "<td>".$result[$i]->getEspecialidad()->getNombre()."</td>";
 				$text.='<td><div class="btn-group btn-group-horizontal">';
-				$text.="<a class='btn btn-info' href='../alumno/".$result[$i]->getNie()->getNie()."'><span class='icon-eye-open icon-white'></span></a>";
-                $text.="<a class='btn btn-info' href='../alumno/".$result[$i]->getNie()->getNie()."/edit'><span class='icon-edit icon-white'></span></a></div></td></tr>";
+				$text.="<a class='btn btn-info' href='../alumno/".$result[$i]->getNie()."'><span class='icon-eye-open icon-white'></span></a>";
+                $text.="<a class='btn btn-info' href='../alumno/".$result[$i]->getNie()."/edit'><span class='icon-edit icon-white'></span></a></div></td></tr>";
 			}
+		}else{
+			$text.="<tr><td colspan='4'>No hay alumnos que concuerden con la busqueda</td></tr>";
 		}
 		$text.= "</table>";
 		//$text.= $query;
 		
 		//Generating the pager for the table
-		$text.= "<div class='pager' id='paginador'>";
-		$text.= "<span class='total'>Total: ".$n_filas." results.</span> ";
+		$text.= "<table class='pager' id='paginador' style='width:95%'><tr>";
+		$text.= "<td style='text-align:left'>Total: ".$n_filas." resultados.</td>";
 		if($n_filas==0) $actual=0;
-		$text.= "Page ".$actual." of ".$n_paginas."  ";
+		$text.= "<td style='text-align:right'>Pagina ".$actual." de ".$n_paginas."  ";
 		if($n_paginas>5){
 			if($actual<=3){
 				if($actual!=1)
@@ -398,7 +398,7 @@ class AlumnoController extends Controller
 					$text.= "<input type='button' value='".$i."' class='actual'>  ";
 			}
 		}
-		$text.= "</div>";
+		$text.= "</td></tr></table>";
 		return new Response($text);
 	}
 }
